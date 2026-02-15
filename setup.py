9d049b0ee94bbc3a03649cda574a8d0e76ac8e82
Jayamanisha <manisharajkumar46@gmail.com>
2026-02-15 14:09:03 +0530

    Add setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DeepfakeVideoDetection',
+    version='0.1',
+    packages=find_packages(),
+    description='A package for detecting deepfake videos',
+    author='manisharajkumar46',
+    author_email='manisharajkumar46@example.com',
+    url='https://github.com/manisharajkumar46/Deepfake-Video-Detection',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
